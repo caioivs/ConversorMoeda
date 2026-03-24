a9c60c493321df73d9976650eeaa003a0132ad1c
Caio Eduardo Nunes da Costa <ce.costa2015@gmail.com>
2026-03-24 20:12:42 -0300

    First Commit
    Corpo do site feito, implementado flask no back-end

---
+++ Conversor/conversor.py
@@ -0,0 +1,37 @@
+import requests
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def obterConvercao():
+    url = "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,GBP-BRL,ARS-BRL"
+    resposta = requests.get(url)
+    dados = resposta.json()
+
+    cotacoes = {
+        "dolar": float(dados["USDBRL"]["bid"]),
+        "euro": float(dados["EURBRL"]["bid"]),
+        "peso": float(dados["ARSBRL"]["bid"]),
+        "libra": float(dados["GBPBRL"]["bid"])
+    }
+    return cotacoes
+
+def converter(real, cotacao):
+    return real / cotacao
+
+@app.route("/", methods=["GET", "POST"])
+def index():           
+        resultado = None
+        moeda = None
+        if request.method == "POST":
+                valor_real = float(request.form["valor_real"])
+                moeda = request.form["moeda"]
+                cotacoes = obterConvercao()
+                resultado = converter(valor_real, cotacoes[moeda])
+            
+        return render_template("index.html", resultado=resultado, moeda=moeda) 
+             
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+     
